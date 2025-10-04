5d0f72761dd2c06c04c26c0f2e173de0f6a811a3
Abdur Rahman <arrahman.lus@gmail.com>
2025-10-04 14:47:55 +0600

    feat: implement random dice roll logic and display dice face images
    - Used Random.secure() to generate dice indices for two dice
    - Calculated dice sum based on indices and displayed result
    - Rendered corresponding dice face images using Image.asset
    - Temporarily used ElevatedButton to trigger dice roll

--- lib/screens/game_page.dart
+++ lib/screens/game_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
+import 'package:dice_roll_game/utils/dice_assets.dart';
+
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
 
@@ -8,6 +12,9 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  int index1 = 0, index2 = 0, diceSum = 0;
+  final random = Random.secure();
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('DICEROLL')),
@@ -15,11 +22,27 @@ class _GamePageState extends State<GamePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('My Dice Game', style: TextStyle(fontSize: 50)),
-            Image.asset('assets/images/d5.png'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(diceList[index1], width: 100, height: 100),
+                const SizedBox(width: 10),
+                Image.asset(diceList[index2], width: 100, height: 100),
+              ],
+            ),
+            Text('Dice Sum: $diceSum', style: const TextStyle(fontSize: 25)),
+            ElevatedButton(onPressed: rollTheDice, child: const Text('ROLL')),
           ],
         ),
       ),
     );
+  }
+
+  void rollTheDice() {
+    setState(() {
+      index1 = random.nextInt(6);
+      index2 = random.nextInt(6);
+      diceSum = index1 + index2 + 2;
+    });
   }
 }
---
+++ lib/utils/dice_assets.dart
@@ -0,0 +1,8 @@
+final diceList = [
+  'assets/images/d1.png',
+  'assets/images/d2.png',
+  'assets/images/d3.png',
+  'assets/images/d4.png',
+  'assets/images/d5.png',
+  'assets/images/d6.png',
+];
